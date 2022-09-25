6800fa44cc4dc69f975387758e5f2c4ec5dd5a98
Ashique20 <ashiqurrahman2092003@gmail.com>
2022-09-25 23:55:11 +0600

    fourth

--- phone.js
+++ phone.js
@@ -21,3 +21,27 @@ const searchPhone = () => {
           console.log(error)
       });
   }
+
+  const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    console.log(phones)
+    phones.forEach(phone => {
+        // console.log(phone)
+        const phDiv = document.createElement('div');
+        phDiv.className = 'card h-100';
+        phDiv.innerHTML = `
+          <img onclick=selectItem(event,"${phone.slug}") src="${phone.image}" class="card-img-top" alt="" />
+          <div class="card-body">
+            <h2 class="card-title">'${phone.phone_name}'</h2>
+            <div class="card-details hide">
+  
+            </div>
+          </div>
+          `;
+        searchResult.appendChild(phDiv);
+    }
+  
+    )
+  
+  }
